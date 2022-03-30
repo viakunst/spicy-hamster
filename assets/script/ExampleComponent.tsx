d99b484b9075750f5aaee049538ff6a3cd0fcc8b
Jasper Weyne <jasperweyne@gmail.com>
2022-03-30 22:33:55 +0200

    Implemented example data fetch

---
+++ assets/script/ExampleComponent.tsx
@@ -0,0 +1,26 @@
+import { gql, GraphQLClient } from 'graphql-request';
+import { usePersonsQuery } from './Api/Backend';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+gql`
+  query Persons {
+    persons {
+      getSub
+    }
+  }
+`;
+
+const ExampleComponent = () => {
+  const endpoint = 'http://localhost:8000/api/graphql/';
+  const client = new GraphQLClient(endpoint, { headers: {} });
+  const {
+    status,
+    data,
+    error,
+    isFetching,
+  } = usePersonsQuery(client, {});
+
+  // ...
+};
+
+export default ExampleComponent;
