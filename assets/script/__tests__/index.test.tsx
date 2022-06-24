bf3e985b72f775b29bce9f87a41d0c147c3ca406
Machiel Kruger <m.a.kruger4@gmail.com>
2022-06-24 21:27:36 +0200

    lint fix

--- assets/script/__tests__/index.test.tsx
+++ assets/script/__tests__/index.test.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+const mock = 'required for no isolated modules';
 
 describe('Basic test', () => {
   test('Always true test.', async () => {
     expect(true).toBe(true);
   });
 });
+
+export default mock;
