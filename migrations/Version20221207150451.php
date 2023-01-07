a558546079080d3d7a8fb76be6de5131d6cfc596
Machiel Kruger <m.a.kruger4@gmail.com>
2023-01-07 14:59:42 +0100

    Update Version20221207150451.php

--- migrations/Version20221207150451.php
+++ migrations/Version20221207150451.php
@@ -20,14 +20,14 @@ final class Version20221207150451 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `transaction` RENAME INDEX idx_f4ab8a06217bbb47 TO IDX_723705D1217BBB47');
-        $this->addSql('ALTER TABLE `transaction` RENAME INDEX idx_f4ab8a06b026f02f TO IDX_723705D1B026F02F');
+        $this->addSql('ALTER TABLE `Transaction` RENAME INDEX idx_f4ab8a06217bbb47 TO IDX_723705D1217BBB47');
+        $this->addSql('ALTER TABLE `Transaction` RENAME INDEX idx_f4ab8a06b026f02f TO IDX_723705D1B026F02F');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `transaction` RENAME INDEX idx_723705d1217bbb47 TO IDX_F4AB8A06217BBB47');
-        $this->addSql('ALTER TABLE `transaction` RENAME INDEX idx_723705d1b026f02f TO IDX_F4AB8A06B026F02F');
+        $this->addSql('ALTER TABLE `Transaction` RENAME INDEX idx_723705d1217bbb47 TO IDX_F4AB8A06217BBB47');
+        $this->addSql('ALTER TABLE `Transaction` RENAME INDEX idx_723705d1b026f02f TO IDX_F4AB8A06B026F02F');
     }
 }
